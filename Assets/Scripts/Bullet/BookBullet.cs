8dd49808ceb498bcba8a435da4d58bcdcc326aab
NK Studio <53283888+NK-Studio@users.noreply.github.com>
2022-12-10 18:23:06 +0900

    책 날아가는거 구현

--- Assets/Scripts/Bullet/BookBullet.cs
+++ Assets/Scripts/Bullet/BookBullet.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using NaughtyAttributes;
 using UnityEngine;
 
 public class BookBullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [ReadOnly] public Vector3 Direction = Vector3.forward;
+    public Transform GFXTransform;
+
+    [Header("생명 주기")]
+    public float LifeTime = 3;
+    
+    [Header("이동 속도")]
+    public float MoveSpeed = 1;
+    
+    [Header("회전 속도")]
+    public float RotateSpeed = 1;
+    
+    private void Start()
     {
-        
+        Destroy(gameObject, LifeTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.Translate(Direction * (MoveSpeed * Time.deltaTime));
+        GFXTransform.Rotate(Vector3.forward * (RotateSpeed * 100 * Time.deltaTime));
     }
 }
